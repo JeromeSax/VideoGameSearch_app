612f54d9f0e4f375bddf5502fa8d1a735a6d979e
unknown <jeromesloss@yahoo.com>
2023-08-06 16:40:10 -0400

    App.js edited

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
 import SearchBar from './components/searchBar';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+const APIKEY = ''
+const [game, setGame] = useState(null)
+
+const getGame = async (searchTerm) => {
+  try {
+    const response = await fetch(``)
+    const data = await response.json();
+    // console.log(data)
+    setGame(data)
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+useEffect(() => {
+  getGame('')
+}, [])
+
   return (
-    <div className="App">
+    <div>
       Hello world!
       <SearchBar />
     </div>
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -4,6 +4,11 @@ const SearchBar = () => {
 
     const [searchInput, setSearchInput] = useState("");
     const videogames = [];
+
+    <form>
+        <span>Search Video Games</span>
+        <button type="submit">Search</button>
+    </form>
 }
 
 export default SearchBar
