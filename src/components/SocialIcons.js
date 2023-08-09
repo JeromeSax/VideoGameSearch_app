6251b67ff85e8257fa11d2676ff790a4c7cb038a
unknown <jeromesloss@yahoo.com>
2023-08-09 13:51:15 -0400

    socialicons being edited

---
+++ src/components/SocialIcons.js
@@ -0,0 +1,8 @@
+import { FaTwitter, FaFacebook } from "react-icons/fa";
+import { StyledSocialIcons } from "./styles/SocialIcons.styled"
+
+export default function SocialIcons() {
+    return (
+        <StyledSocialIcons></StyledSocialIcons>
+    )
+}
