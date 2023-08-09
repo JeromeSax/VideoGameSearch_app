fddaae0f5ac8550c3e86066106d078d6eca29d24
unknown <jeromesloss@yahoo.com>
2023-08-09 11:53:35 -0400

    footer and react icons added

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import { Container } from "./styles/Container.styled";
+import { StyledFooter } from "./styles/Footer.styled";
+
+
+export default function Footer() {
+    return (
+        <StyledFooter>
+            <Container>
+                
+            </Container>
+        </StyledFooter>
+    )
+}
