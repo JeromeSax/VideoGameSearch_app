5f11294eb6afb9419c2b55f9eefb814758ee0575
unknown <jeromesloss@yahoo.com>
2023-08-10 09:18:19 -0400

    videogameDisplay edited

--- src/App.js
+++ src/App.js
@@ -11,13 +11,11 @@ const Container = styled.div`
   margin: auto;
   text-align: center;
   background-color: lightgreen;
-  font-size: 14px;
+  font-size: 12px;
   border-radius: 10px 10px 10px 10px;
   box-shadow:  0 0 20px #FF0000, 0 0 10px #0000FF;
-
-  h1 {
-    text-shadow: 0 0 3px #FF0000, 0 0 5px #0000FF;
-  }
+  text-shadow: 0 0 3px #FF0000, 0 0 5px #0000FF;
+  
 `
 
 function App() {
@@ -26,78 +24,12 @@ function App() {
     searchTerm: ''
 });
 
-  const [game, setGame] = useState(
-    {
-      "_id": "5d866f6993765d6778740b40",
-      "magic": 5320,
-      "isChosen": false,
-      "title": null,
-      "publishedDate": "2015-07-12T04:00:00.000Z",
-      "externalUrl": "http://www.forbes.com/sites/games/2015/07/12/the-witcher-3-wild-hunt-review-into-the-great-wide-open/",
-      "snippet": "All told, I give the game a Buy in my Buy/Hold/Sell ranking. For all its flaws—and I realize that I've listed many—it is one of the most sprawling, magnificent solo RPGs ever made. It does too little to fix its mechanical problems, and focuses too much on creating an open world, but the quality of its quest design, world-building, and wonderful (if too large) cast of characters makes it a game that no RPG fan should miss.\n\nIt may be a flawed masterpiece, but it's a masterpiece nonetheless.",
-      "language": "en-us",
-      "score": 90,
-      "npScore": 100,
-      "isQuoteManual": false,
-      "Authors": [
-        {
-          "_id": "5d866f6993765d6778740b41",
-          "id": 1158,
-          "name": "Erik Kain",
-          "image": true,
-          "imageSrc": {
-            "og": "critic/1158/o/hgV2ohtH.jpg",
-            "sm": "critic/1158/eYp2RDH6.jpg",
-            "lg": "critic/1158/OnhjUh6t.jpg"
-          }
-        }
-      ],
-      "ScoreFormat": {
-        "id": 18,
-        "name": "0 to 10 incl decimals",
-        "shortName": "x.x / 10.0",
-        "scoreDisplay": " / 10.0",
-        "isNumeric": true,
-        "isSelect": false,
-        "isStars": false,
-        "numDecimals": 1,
-        "base": 10,
-        "options": null
-      },
-      "Platforms": [
-        {
-          "_id": "5d866f6993765d6778740b42",
-          "id": 27,
-          "name": "PC",
-          "shortName": "PC"
-        }
-      ],
-      "alias": "Erik Kain",
-      "game": {
-        "id": 463,
-        "name": "The Witcher 3: Wild Hunt"
-      },
-      "Outlet": {
-        "name": "Forbes",
-        "isContributor": false,
-        "id": 290,
-        "imageSrc": {
-          "og": "outlet/290/o/wFV4sPVC.jpg",
-          "sm": "outlet/290/wOGqziyy.jpg",
-          "lg": "outlet/290/UeHhhlAS.jpg"
-        }
-      },
-      "createdAt": "2019-09-21T18:43:53.518Z",
-      "updatedAt": "2022-08-11T16:47:28.090Z",
-      "__v": 0,
-      "blend_magic": 5320
-    }
-
-
-  )
+  const [game, setGame] = useState([])
 
   const getGames = async (searchTerm) => {
-    const url = `https://opencritic-api.p.rapidapi.com/reviews/game/463?skip=20`;
+    
+  
+    const url = `https://opencritic-api.p.rapidapi.com/game/search?criteria=${searchTerm}`;
     const options = {
       method: 'GET',
       headers: {
@@ -110,6 +42,7 @@ function App() {
       const response = await fetch(url, options);
       const result = await response.json();
       console.log(result);
+      setGame(result)
     } catch (error) {
       console.error(error);
     }
@@ -118,14 +51,14 @@ function App() {
 
 
   // useEffect(() => {
-  //   getGames({game})
+  //   getGames()
   // }, [])
 
 
  
 
 const handleChange = (e) => {
-    console.log(e.target.value)
+    // console.log(e.target.value)
     setForm ({
         ...form,
         searchTerm: e.target.value
@@ -134,7 +67,7 @@ const handleChange = (e) => {
 
 const handleSubmit = (e) => {
     e.preventDefault();
-    getGames(form.searchTerm);
+    getGames();
     form.searchTerm= "";
 }
 
--- src/components/VideogameDisplay.js
+++ src/components/VideogameDisplay.js
@@ -2,21 +2,29 @@ import React from 'react'
 
 function VideogameDisplay({game}) {
 
-  const loaded =() => {
-    return (
-      <div>
-        {/* <h1>{game.Authors[0].name}</h1> */}
-        {/* <h1>{game._id}</h1> */}
-        <h1>{game.snippet}</h1>
-      </div>
-    );
-  };
+  // const loaded =() => {
+  //   return (
+  //     <div>
+  //       <h1>{game.name}</h1>
+  //     </div>
+  //   );
+  // };
 
-  const loading = () => {
-    return <h1>No Game to Display</h1>
-  }
+  // const loading = () => {
+  //   return <h1>No Game to Display</h1>
+  // }
+// console.log(game)
+// return game ? loaded() : loading()
 
-  return game? loaded() : loading()
-}
+return (
+    game.map((game, index) => {
+      return (
+        <ul>
+          <li>{game.name}</li>
+        </ul>
+      )
+    }
+  )
+)}
 
 export default VideogameDisplay
